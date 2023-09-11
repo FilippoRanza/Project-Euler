ea27bccb1fb4a3984946d67a5471b051367509e9
Filippo Ranza <filipporanza@gmail.com>
2023-09-11 23:27:35 +0200

    Add problem 57

--- src/c/digit_utils.h
+++ src/c/digit_utils.h
@@ -1,6 +1,8 @@
 #ifndef __DIGIT_UTILS_H
 #define __DIGIT_UTILS_H
 
+#include <gmp.h>
+
 void digit_count(int number, int *ptr) {
     while (number) {
         ptr[number % 10]++;
@@ -14,6 +16,17 @@ int number_len(int number) {
         output++;
         number /= 10;
     }
+    return output;
+}
+
+
+unsigned long big_number_len(mpz_t n) {
+    unsigned long output = 0;
+    while (mpz_cmp_ui(n, 0) != 0) {
+        output++;
+        mpz_fdiv_q_ui(n, n, 10);
+    }
+
     return output;
 }
 
